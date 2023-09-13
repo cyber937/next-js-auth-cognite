84de3147341c717dc61d9eada904a5269b057b1c
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2023-09-13 11:49:19 -0600

    Show use's email when signed in.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,25 +5,23 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 export default function Nav() {
   const { data: session } = useSession()
 
-  let authButton: React.ReactElement | null = null;
-
-  if (!session) {
-    authButton = (
+  if (session) {
+    return (
+      <div>
+        <div>
+          Singed in as {session.user?.email}
+        </div>
+        <button onClick={() => signOut({ callbackUrl: process.env.NEXT_PUBLIC_DOMAIN })}>
+          Sign Out
+        </button>
+      </div>
+    )
+  }
+  return (
+    <div>
       <button onClick={() => signIn("cognito")}>
         Sign In
       </button>
-    )
-  } else {
-    authButton = (
-      <button onClick={() => signOut({ callbackUrl: process.env.NEXT_PUBLIC_DOMAIN })}>
-        Sign Out
-      </button>
-    )
-  }
-
-  return (
-    <div>
-      {authButton}
     </div>
   )
 }
