4e4acb005d6deb9dcee9b0946a3c132e8f1f44d1
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2023-09-13 11:27:41 -0600

    Using 'withAuth' wrap for the advanced usage.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,17 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware(req) {
+    if (process.env.NODE_ENV === "development") {
+      console.log(req.nextauth.token);
+    }
+  },
+  {
+    // Pages
+    pages: {
+      signIn: "/",
+    },
+  }
+);
 
 export const config = { matcher: ["/dashboard"] };
